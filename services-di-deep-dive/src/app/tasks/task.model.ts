3d036f12a3790c2fb9d7196e512af6f4f388df34
jlgallego99 <joseluis.gallego@ageo.es>
2024-12-02 14:36:09 +0100

    Provide non-class in injection

--- services-di-deep-dive/src/app/tasks/task.model.ts
+++ services-di-deep-dive/src/app/tasks/task.model.ts
@@ -1,4 +1,35 @@
+import { InjectionToken, Provider } from "@angular/core";
+
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
+
+type TaskStatusOptions = {
+  value: 'open' | 'in-progress' | 'done',
+  taskStatus: TaskStatus,
+  text: string,
+}[];
+export const TASK_STATUS_OPTIONS = new InjectionToken<TaskStatusOptions>('task-status-options');
+export const TaskStatusOption: TaskStatusOptions = [
+  {
+    value: 'open',
+    taskStatus: 'OPEN',
+    text: 'Open'
+  },
+  {
+    value: 'in-progress',
+    taskStatus: 'IN_PROGRESS',
+    text: 'In-Progress'
+  },
+  {
+    value: 'done',
+    taskStatus: 'DONE',
+    text: 'Completed'
+  },
+];
+
+export const taskStatusOptionsProvider: Provider = {
+  provide: TASK_STATUS_OPTIONS,
+  useValue: TaskStatusOption,
+};
 
 export interface Task {
   id: string;
