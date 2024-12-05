40f676c6f1b84048d224d5e3622c599a3cebe362
jlgallego99 <joseluis.gallego@ageo.es>
2024-12-05 09:18:06 +0100

    Rxjs project

---
+++ rxjs-deep-dive/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { interval, map } from "rxjs";
+import { toObservable } from "@angular/core/rxjs-interop";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
+
+  constructor() {
+    // effect(() => {
+    //   console.log(`Clicked button ${this.clickCount()} times.`)
+    // });
+  }
+
+  ngOnInit(): void {
+    // const subscription = interval(1000).pipe(
+    //   map((val) => val * 2)
+    // ).subscribe({
+    //   next: (val) => console.log(val),
+    // });
+    //
+
+    const subscription = this.clickCount$.subscribe({
+      next: () => () => console.log(`Clicked button ${this.clickCount()} times.`)
+    })
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1)
+  }
+}
