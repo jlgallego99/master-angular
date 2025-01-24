f8d8f5caa1f9c5987420b6b150f0f259424ac9da
jlgallego99 <joseluis.gallego@ageo.es>
2025-01-24 13:25:15 +0100

    Dynamic routes

--- multi-page/src/app.routes.ts
+++ multi-page/src/app.routes.ts
@@ -1,9 +1,15 @@
 import { TasksComponent } from "./app/tasks/tasks.component";
 import { Routes } from "@angular/router";
+import { NoTaskComponent } from "./app/tasks/no-task/no-task.component";
+import { UserTasksComponent } from "./app/users/user-tasks/user-tasks.component";
 
 export const appRoutes: Routes = [
   {
-    path: 'tasks',
-    component: TasksComponent
+    path: '',
+    component: NoTaskComponent,
   },
+  {
+    path: 'users/:userId',
+    component: UserTasksComponent,
+  }
 ]
